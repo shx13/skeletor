fd0405ca8a103099b7f4c2c038a5496eadf8fa19
shx13 <shx420@gmail.com>
2015-08-30 19:20:11 +0100

    Javascript code moved to footer.

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,2 +1,15 @@
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+<?php
+
+  // add extra javascript (declared in the controller file)
+  if(isset($this->extraJS)) {
+    foreach ($this->extraJS as $xjs) {
+      echo '<script src="'.Config::get('URL').'pub/js/'.$xjs.'"></script>'."\r\n";
+    }
+  }
+
+?>
+
 </body>
 </html>
--- app/view/common/header.php
+++ app/view/common/header.php
@@ -17,17 +17,7 @@
       echo '<link rel="stylesheet" href="'.Config::get('URL').'pub/css/'.$xcss.'">'."\r\n";
     }
   }
-
-  // add extra javascript (declared in the controller file)
-  if(isset($this->extraJS)) {
-    foreach ($this->extraJS as $xjs) {
-      echo '<script src="'.Config::get('URL').'pub/js/'.$xjs.'"></script>'."\r\n";
-    }
-  }
-
 ?>
-
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 
 </head>
 <body>
